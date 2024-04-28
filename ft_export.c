861669aeb1a074e2835a8447bff28ad9c04f7e4f
cherrynuna <114652843+cherrynuna@users.noreply.github.com>
2024-04-28 02:42:30 +0000

    0428export

--- ft_export.c
+++ ft_export.c
@@ -33,14 +33,26 @@ void    ft_add_var(char *key, t_list *cpenv)
 
 int ft_export(t_pars_tree *cmd, t_list cpenv, int fd)
 {
+    t_list	*cur;
+
+	open(fd, O_WRONLY);
 	if (cmd->cmd && cmd->redir_flag == 0)
 	{
-		if (ft_valid_key(cmd->cmd[1]))
+		if (!cmd->cmd[1])//no opt, no arg
+        {
+        	cur = cpenv;
+	        while (cur)
+	        {
+		        ft_putendl_fd(cur->content, fd);
+		        cur = cur->next;
+            }
+            return (1);
+        }
+        if (ft_valid_key(cmd->cmd[1]))
 			ft_add_var(cmd->cmd[1], cpenv);
 		else
 			ft_putendl_fd("bash: unset: `a=': not a valid identifier", 2);
 			return (-1);
 	}
-	open(fd, O_WRONLY);
 	return (1);
 }
